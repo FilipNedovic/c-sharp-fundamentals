1f4494cc7d23a6c4c30c1fb177b6e49229257669
Filip Nedovic <Filip_Nedovic@epam.com>
2022-12-30 11:15:09 +0100

    added unit tests

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -1,6 +1,6 @@
 namespace GradeBook
 {
-    class Book
+    public class Book
     {
         // Explicit constructor
         public Book(string name)
@@ -14,22 +14,22 @@ namespace GradeBook
             grades.Add(grade);
         }
 
-        public void ShowStatistics()
+        public Statistics GetStatistics()
         {
-            var result = 0.0;
-            var highGrade = double.MinValue;
-            var lowGrade = double.MaxValue;
-            foreach (double number in grades)
+            var result = new Statistics();
+            result.Average = 0.0;
+            result.High = double.MinValue;
+            result.Low = double.MaxValue;
+            foreach (double grade in grades)
             {
-                highGrade = Math.Max(number, highGrade);
-                lowGrade = Math.Min(number, lowGrade);
-                result += number;
+                result.High = Math.Max(grade, result.High);
+                result.Low = Math.Min(grade, result.Low);
+                result.Average += grade;
             }
 
-            result /= grades.Count;
-            Console.WriteLine($"The lowest grade is {lowGrade}");
-            Console.WriteLine($"The highest grade is {highGrade}");
-            Console.WriteLine($"The average grade is {result:N1}");
+            result.Average /= grades.Count;
+
+            return result;
         }
 
         // Field definition
